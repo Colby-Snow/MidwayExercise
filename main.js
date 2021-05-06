f91aba8bacc5f3a252b0c4f5977dc7afa6122b66
Iceflame21 <19cmsnow@gmail.com>
2021-05-06 15:53:12 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,76 @@
+function validateTimeForm(){
+    console.log("ValidateTimeForm was called");
+    var time = $("#timeinput").val();
+    var inputedTime = time.split(':');
+    var hours = parseInt(inputedTime[0]);
+    var minutes = parseInt(inputedTime[1]);
+    console.log(`${hours} hours and ${minutes} minutes`);
+    if(hours > 12){
+        var pm = true;
+        hours = hours - 12;
+    }
+    var timeInWords = convertTimeToWords(hours,minutes);
+    displayTime(timeInWords);
+    event.preventDefault();
+}
+
+function runTestScenarios(){
+    let times = [
+        [5,47],
+        [3,0],
+        [7,29],
+        [5,30],
+        [5,45],
+        [4,15],
+        [6,35],
+        [3,30],
+        [10,57],
+        [1,1],
+        [7,15],
+        [12,45]
+    ];
+    times.forEach(convertArrayTimeToWords);
+    event.preventDefault();
+}
+
+function convertArrayTimeToWords(item, index){
+    var timeInWords = convertTimeToWords(item[0], item[1]);
+    displayTime(timeInWords);
+}
+
+function convertTimeToWords(hours, minutes){
+    var hourString, minString = "";
+
+    //Sets the text needed for the hour and minute into two arrays
+    var hoursArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "one"];
+    var minuteArray = ["one minute", "two minutes","three minutes","four minutes","five minutes","six minutes","seven minutes","eight minutes","nine minutes", 
+                        "ten minutes", "eleven minutes", "twelve minutes", "thirteen minutes", "fourteen minutes", "quarter", "sixteen minutes", "seventeen minutes", "eighteen minutes", "nineteen minutes",
+                        "twenty minutes", "twenty-one minutes", "twenty-two minutes","twenty-three minutes","twenty-four minutes","twenty-five minutes","twenty-six minutes","twenty-seven minutes","twenty-eight minutes", "twenty-nine minutes", "half"];
+    hourString = hoursArray[hours-1];
+
+    //logic needed:
+    //At 00 minutes must say o'clock
+    //When the minutes are less than or equal to 30 format is "minutes past hour"
+    //When the minutes are greater than 30 format is "(60-minutes) to hour"
+
+    if(minutes == 0){
+        var resultString = `The time is ${hourString} o'clock`;
+    }
+    else if(minutes <= 30){
+        minString = minuteArray[minutes-1];
+        var resultString = `The time is ${minString} past ${hourString} `;
+    }
+    else{
+        var inverseMinutes = 60 - minutes;
+        minString = minuteArray[inverseMinutes-1];
+        hourString = hoursArray[hours];
+        var resultString = `The time is ${minString} to ${hourString}`;
+    }
+
+    return resultString;
+}
+
+function displayTime(timeInWords){
+
+    $("form").after("<div class=timeCard><h2>" + timeInWords + "</h2></div>");
+}
